434a37d7c4a5b365df672e95939958fa30805bf5
Shulaev Ivan <mail@ntdservice.ru>
2023-02-08 17:25:33 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './js/app';
 import './css/style.css';
 import './components/collapse/collapse';
-import './components/collapse/collapse.css'
+import './components/collapse/collapse.css';
+import './components/callback/callback.js';
+import './components/callback/callback.css';
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import Collapse from '../components/collapse/collapse';
+import Callback from '../components/callback/callback';
 
 window.addEventListener('DOMContentLoaded', (e) => {
     const collapse = new Collapse('.app-container-collapse');
+    const callback = new Callback();
 });

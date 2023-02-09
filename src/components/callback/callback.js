7cb6cebf26c0a9650e84f430f35b13aa9ebebd5f
Shulaev Ivan <mail@ntdservice.ru>
2023-02-09 17:23:12 +0300

    second task add

--- src/components/callback/callback.js
+++ src/components/callback/callback.js
@@ -1,5 +1,36 @@
 export default class Callback {
-    constructor() {
+    constructor(callbackTag, popupTag) {
+        this.callbackTag = document.querySelector(callbackTag);
+        this.popupTag = document.querySelector(popupTag);
+        this.callbackBtn = this.callbackTag.querySelector('.callback-btn');
+        this.popupSubmitBtn = this.popupTag.querySelector('.callback-submit-btn');
+        this.popupCloseBtn = this.popupTag.querySelector('.callback-close-btn');
+
+        this.popupTag.classList.add('callback-popup-hide');
+        this.callbackBtn.addEventListener('click', this.showPopup);
+        this.popupCloseBtn.addEventListener('click', this.hidePopup);
+
+    }
+
+    showPopup = (e) => {
+        const target = e.target;
+        if (target && target.classList.contains('callback-btn')) {
+            this.popupTag.classList.remove('callback-popup-none');
+            this.callbackBtn.classList.add('hidden');
+            this.popupTag.style.top = `${target.getBoundingClientRect().top}px`;
+            this.popupTag.style.left = `${target.getBoundingClientRect().left + 150}px`;
+            this.popupTag.classList.add('callback-popup-show');
+            this.popupTag.classList.remove('callback-popup-hide');
+        }
         
+    }
+
+    hidePopup = (e) => {
+        this.callbackBtn.classList.remove('hidden');
+        this.popupTag.classList.add('callback-popup-hide');
+        this.popupTag.classList.remove('callback-popup-show');
+        setTimeout(() => {
+            this.popupTag.classList.remove('callback-popup-none');
+        }, 300)
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,5 @@ import Callback from '../components/callback/callback';
 
 window.addEventListener('DOMContentLoaded', (e) => {
     const collapse = new Collapse('.app-container-collapse');
-    const callback = new Callback();
+    const callback = new Callback('.callback-main-wrap', '.callback-popup-wrap');
 });
